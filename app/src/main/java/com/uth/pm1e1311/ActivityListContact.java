8b4fdbf12a8c277c09deb121a202bef9e4ad63c4
etorrres <79731703+etorrres@users.noreply.github.com>
2024-02-15 22:46:26 -0600

    Pantallas de Lista de contacto y Editar contacto

---
+++ app/src/main/java/com/uth/pm1e1311/ActivityListContact.java
@@ -0,0 +1,14 @@
+package com.uth.pm1e1311;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityListContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_contact);
+    }
+}
