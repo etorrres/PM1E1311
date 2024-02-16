51a6e13b07b16b91f47f6a15b1a8fbd0ecb1505b
Edisson <edisson072011@gmail.com>
2024-02-15 23:18:22 -0600

    Metodo de crear contacto

--- app/src/main/java/com/uth/pm1e1311/Configuracion/Transacciones.java
+++ app/src/main/java/com/uth/pm1e1311/Configuracion/Transacciones.java
@@ -10,25 +10,18 @@ public class Transacciones
     public static final String nombre = "nombre";
     public static final Long telefono = Long.valueOf("telefono");
     public static final String nota = "nota";
-    public static final String imagen_id = "imagen_id";
 
-    // ESTRUCTURA DE LA TABLA IMAGEN
-    public static final String TablaImagenes = "imagen";
-    public static final String id_imagen = "id_imagen";
     public static final String imagen = "imagen";
 
     //CREACION
     public static final String CreateTableContactos = "Create table "+ TablaContactos +" ("+
-            "id_contacto PRIMARY KEY AUTOINCREMENT, nombre TEXT, telefono LONG, nota TEXT)";
-    public static final String CreateTableImagen = "Create table "+ TablaImagenes +" ("+
-            "id_imagen PRIMARY KEY AUTOINCREMENT, imagen BLOB)";
+            "id_contacto PRIMARY KEY AUTOINCREMENT, nombre TEXT, telefono LONG, nota TEXT, imagen BLOB)";
 
     //DROP
     public static final String DropTableContactos = "DROP TABLE IF EXISTS "+TablaContactos;
-    public static final String DropTableImagenes = "DROP TABLE IF EXISTS "+TablaImagenes;
 
     //SELECT
     public static final String SelectAllContactos = "SELECT * FROM "+ TablaContactos;
-    public static final String SelectAllImagenes = "SELECT * FROM "+ TablaImagenes;
+
 
 }
--- app/src/main/java/com/uth/pm1e1311/MainActivity.java
+++ app/src/main/java/com/uth/pm1e1311/MainActivity.java
@@ -2,13 +2,79 @@ package com.uth.pm1e1311;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.uth.pm1e1311.Configuracion.SQLiteConexion;
+import com.uth.pm1e1311.Configuracion.Transacciones;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nombre, telefono, nota;
+    Button btn_salvarContacto, btn_contactosSalvados;
+
+    String mensaje="";
+
+    Boolean validacion = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombre = (EditText) findViewById(R.id.text_nombre);
+        telefono = (EditText) findViewById(R.id.text_telefono);
+        nota = (EditText) findViewById(R.id.text_notas);
+
+        btn_salvarContacto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RegistrarContacto();
+            }
+        });
+
+    }
+    private void RegistrarContacto()
+    {
+        SQLiteConexion conn = new SQLiteConexion(this, Transacciones.DBName, null, 1);
+        SQLiteDatabase db = conn.getWritableDatabase();
+
+        ContentValues valores = new ContentValues();
+        valores.put(Transacciones.nombre, nombre.getText().toString());
+        valores.put(String.valueOf(Transacciones.telefono), telefono.getText().toString());
+        valores.put(Transacciones.nota, nota.getText().toString());
+
+        if (nombre == null){
+          mensaje = "Debes ingresar un nombre";
+          validacion = false;
+        } else{
+            if (telefono == null){
+                mensaje = "Debes ingresar un telefono";
+                validacion = false;
+            }else{
+                if (nota == null){
+                    mensaje = "Debes ingresar una nota";
+                    validacion = false;
+                }else{
+                    mensaje = "Contacto Registrado Correctamente";
+                    validacion = true;
+                }
+            }
+        }
+
+        if(validacion){
+            Long result = db.insert(Transacciones.TablaContactos, Transacciones.id_contacto, valores);
+
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+
+            db.close();
+        }else{
+            Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
