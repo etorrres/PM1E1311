6204d07b2a34982e12aced8c2ca9521e74a5aa53
etorrres <79731703+etorrres@users.noreply.github.com>
2024-02-16 21:47:53 -0600

    Actualizacion de contacto y eliminacion de contacto

--- app/src/main/java/com/uth/pm1e1311/MainActivity.java
+++ app/src/main/java/com/uth/pm1e1311/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -20,10 +19,9 @@ public class MainActivity extends AppCompatActivity {
     EditText nombre, telefono, nota;
     Button btn_salvarContacto, btn_contactosSalvados;
 
-    String mensaje="", valorPais="";
+    String  valorPais="";
     Spinner spinnerPaises;
 
-    Boolean validacion = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,57 +34,60 @@ public class MainActivity extends AppCompatActivity {
         btn_salvarContacto = (Button) findViewById(R.id.btn_salvarContacto);
         btn_contactosSalvados = (Button) findViewById(R.id.btn_contactosSalvados);
 
-        btn_salvarContacto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String nombreTxt = nombre.getText().toString().trim();
-                String telefonoTxt = telefono.getText().toString().trim();
-                String notaTxt = nota.getText().toString().trim();
+        btn_salvarContacto.setOnClickListener(v -> {
+            String nombreTxt = nombre.getText().toString().trim();
+            String telefonoTxt = telefono.getText().toString().trim();
+            String notaTxt = nota.getText().toString().trim();
 
-                if (nombreTxt.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Debe escribir un nombre",
-                            Toast.LENGTH_SHORT).show();
-                } else if (telefonoTxt.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Debe escribir un numero de telefono",
-                            Toast.LENGTH_SHORT).show();
-                } else if (notaTxt.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Debe escribir una nota",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    RegistrarContacto();
-                }
+            if (nombreTxt.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Debe escribir un nombre",
+                        Toast.LENGTH_SHORT).show();
+            } else if (telefonoTxt.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Debe escribir un numero de telefono",
+                        Toast.LENGTH_SHORT).show();
+            } else if (notaTxt.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "Debe escribir una nota",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                RegistrarContacto();
             }
         });
 
-        btn_contactosSalvados.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ActivityListContact.class);
-                startActivity(intent);
-            }
+        btn_contactosSalvados.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), ActivityListContact.class);
+            startActivity(intent);
         });
 
     }
     private void RegistrarContacto()
     {
-        SQLiteConexion conn = new SQLiteConexion(this, Transacciones.DBName, null, 1);
-        SQLiteDatabase db = conn.getWritableDatabase();
+        SQLiteDatabase db;
+        try (SQLiteConexion conn = new SQLiteConexion(this, Transacciones.DBName, null, 1)) {
+            db = conn.getWritableDatabase();
+        }
 
         String paisSeleccionado = spinnerPaises.getSelectedItem().toString();
 
 
-        if (paisSeleccionado.equals("Honduras (504)")) {
-            valorPais = "504";
-        } else if (paisSeleccionado.equals("Belice (501)")) {
-            valorPais = "501";
-        } else if (paisSeleccionado.equals("Guatemala (502)")) {
-            valorPais = "502";
-        } else if (paisSeleccionado.equals("Nicaragua (505)")) {
-            valorPais = "505";
-        } else if (paisSeleccionado.equals("Costa Rica (506)")) {
-            valorPais = "506";
-        } else if (paisSeleccionado.equals("Pánama (507)")) {
-            valorPais = "507";
+        switch (paisSeleccionado) {
+            case "Honduras (504)":
+                valorPais = "504";
+                break;
+            case "Belice (501)":
+                valorPais = "501";
+                break;
+            case "Guatemala (502)":
+                valorPais = "502";
+                break;
+            case "Nicaragua (505)":
+                valorPais = "505";
+                break;
+            case "Costa Rica (506)":
+                valorPais = "506";
+                break;
+            case "Pánama (507)":
+                valorPais = "507";
+                break;
         }
 
         ContentValues valores = new ContentValues();
